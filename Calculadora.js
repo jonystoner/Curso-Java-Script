421aee9fec66b524b2654e9a5e7d1f4f93537715
jonystoner <74501970+jonystoner@users.noreply.github.com>
2021-03-24 20:51:56 -0300

    Alterações no codigo na function de calcule e no html no input

--- Calculadora.js
+++ Calculadora.js
@@ -1,4 +1,21 @@
-function(botao){
+var zero = "0"
+var salvo
+var resultado 
+function botao(num){
+   salvo = document.calc.visor.value += num
 
+}
+
+function reset(){
+
+   document.calc.visor.value += num
+   
+}
+
+function calcule(){
+
+resultado = eval(salvo);
+
+document.calc.visor.value = resultado 
 
 }
