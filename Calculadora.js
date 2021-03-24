7e8acbb1d84cbd37a2138209a0c6ce726664b47e
jonystoner <74501970+jonystoner@users.noreply.github.com>
2021-03-24 15:57:46 -0300

    Novas alterações

--- Calculadora.js
+++ Calculadora.js
@@ -1,4 +1,7 @@
-function botao()
-{
-    
-}
+const visor = document.getElementById('visor');
+
+const numeros = document.querySelectorAll('[id*=tecla]')
+
+const inserirnumero =(evento) => visor.textContent = evento.target.textContent;
+
+numeros.forEach(numero => numero.addEventListener('click',inserirnumero) );
